2d318e51db6de90bba1c8834eb187c8673733937
Jomsk <69858668+Jomsk@users.noreply.github.com>
2020-08-24 22:42:01 -0500

    Update index.js

--- static/js/index.js
+++ static/js/index.js
@@ -22,13 +22,13 @@ function LED1_Off(){
 // Create a client instance
   //client = new Paho.MQTT.Client("postman.cloudmqtt.com", 14970);
   
-  client = new Paho.MQTT.Client("maqiatto.com", 1883, "web_" + parseInt(Math.random() * 100, 10));
+  client = new Paho.MQTT.Client("maqiatto.com", 8883, "web_" + parseInt(Math.random() * 100, 10));
 
   // set callback handlers
   client.onConnectionLost = onConnectionLost;
   client.onMessageArrived = onMessageArrived;
   var options = {
-   useSSL: false,
+   useSSL: true,
     userName: "jomsk@hotmail.com",
     password: "Jomsk4all1996",
     onSuccess:onConnect,
